4ba9d0df41d4738216f40f1edc3a9802e549eaa1
JonMinton <nate.minton@gmail.com>
2015-09-25 11:29:23 +0100

    first proj commit

---
+++ script.R
@@ -0,0 +1,32 @@
+# Main scripts 
+
+# Tasks - load contents from dropbox linkes
+
+# Prereqs -----------------------------------------------------------------
+
+
+
+rm(list = ls())
+
+require(repmis)
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+
+
+
+# Comments & links from Gavin ---------------------------------------------
+
+
+# Jon, the RData you asked are in the following link. 
+# Because I did the analysis city by city, so there are a lot objects in each RData. 
+# I think the object "indicators.post" should include the information you want. Tell me if you want something else.
+# 
+# https://www.dropbox.com/s/bpa0kyie7eomsyj/aberdeen_analysis.RData?dl=0
+# https://www.dropbox.com/s/s9s5j3zrevmp3qu/edinburgh_analysis.RData?dl=0
+# https://www.dropbox.com/s/h769s8mslaqu5r3/glasgow_analysis.RData?dl=0
+# https://www.dropbox.com/s/3y1nitve9vb0v8k/london_analysis.RData?dl=0
+# https://www.dropbox.com/s/02no8ms3twkoqwl/manchester_analysis.RData?dl=0
+
+
+
